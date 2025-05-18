4292d0222f26925d34f58919a00982496ada3905
Midnight <luisgabrielle1026@gmail.com>
2025-05-18 21:16:49 +0800

    add: storing token in firestore

--- nurse-joy-api/src/app/api/route.ts
+++ nurse-joy-api/src/app/api/route.ts
@@ -1,8 +1,10 @@
-import { RtcTokenBuilder, RtcRole } from "agora-access-token";
+import { RtcTokenBuilder, RtcRole } from "agora-token";
 import { NextRequest, NextResponse } from "next/server";
+import { firestore} from "@/utils/firebase";
+import { doc, setDoc, collection } from "firebase/firestore/lite";
 
 export async function POST(req: NextRequest) {
-  const { channelName, uid } = await req.json();
+  const { chatRoomId, channelName, uid } = await req.json();
 
   if (!channelName || !uid) {
     return NextResponse.json({ error: "Channel name and uid are required" }, { status: 400 });
@@ -11,6 +13,7 @@ export async function POST(req: NextRequest) {
 
   const currentTime = Math.floor(Date.now() / 1000);
   const expirationTime = currentTime + 60 * 60 * 2; // 2 hours
+  const privilegeExpiredTs = currentTime + 60 * 60 * 2;
 
   const token = RtcTokenBuilder.buildTokenWithUid(
     process.env.AGORA_APP_ID!,
@@ -18,8 +21,26 @@ export async function POST(req: NextRequest) {
     channelName,
     uid,
     RtcRole.PUBLISHER,
-    expirationTime
+    expirationTime,
+    privilegeExpiredTs,
   );
 
+  await storeTokenFireStore(chatRoomId, channelName, uid, token, privilegeExpiredTs);
+
   return NextResponse.json({ token }, { status: 200 });
+}
+
+async function storeTokenFireStore(chatRoomId: string, channelName: string, uid: number, token: string, expiresAt: number) {
+    // store the token in video-call collection in firestore
+    
+    const videoCallRef = collection(firestore, 'video-call');
+    const docRef = doc(videoCallRef, channelName);
+    await setDoc(docRef, {
+        'chatRoomId': chatRoomId,
+        'channelName': channelName,
+        'uid': uid,
+        'token': token,
+        'expiresAt': expiresAt,
+        'createdAt': new Date(),
+    });
 }
