14bd7fb911eaa145fe798b52dec4a282561d5e63
Midnight <luisgabrielle1026@gmail.com>
2025-06-17 22:33:13 +0800

    eliminated messaging in web-firebase

--- src/utils/web-firebase.ts
+++ src/utils/web-firebase.ts
@@ -1,8 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getMessaging } from "firebase/messaging";
+import { getFirestore } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,4 +20,3 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const webFirestore = getFirestore(app);
-export const webMessaging = getMessaging(app);
