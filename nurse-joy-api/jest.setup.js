f3910a0075f7c3efcf91ad38c807786c8ceb87fc
Luis Gabrielle Putan <luisgabrielle1026@gmail.com>
2025-06-17 21:48:52 +0800

    Delete nurse-joy-api directory

--- nurse-joy-api/jest.setup.js
+++
@@ -1,9 +0,0 @@
-// Set NODE_ENV for tests
-process.env.NODE_ENV = 'test';
-
-// Learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-
-// Mock environment variables
-process.env.AGORA_APP_ID = 'test-app-id';
-process.env.AGORA_APP_CERTIFICATE = 'test-app-certificate'; 
